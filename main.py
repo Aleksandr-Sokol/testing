5f70ecd6b5c7a116af16a9e2d89d3c5dc11e01fc
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2022-01-22 10:47:43 +0500

    Create linear_function

---
+++ main.py
@@ -0,0 +1,9 @@
+from symple_calc import main_calc, linear_function
+
+
+def main():
+    print('start calcutations')
+    value = main_calc(10, linear_function)
+    print(f'value is {value}')
+
+main()
---
+++ symple_calc.py
@@ -0,0 +1,15 @@
+def linear_function(x: float) -> float:
+    """
+    f(x) = x + 1
+    """
+    return x + 1
+
+
+def main_calc(x: float, f) -> float:
+    """"
+    :param float x : abcissa
+    :param f : function
+    return f(x)
+    """
+    value = f(x)
+    return value
